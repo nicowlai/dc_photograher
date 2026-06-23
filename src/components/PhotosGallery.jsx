37b89f869b0ec50e2a2f5e1c7d484b23745f10c2
Nicole Lai <plain.nicole31@gmail.com>
2026-06-24 04:11:31 +1200

    git commit -m "feat(portfolio): add photo gallery display component"

---
+++ src/components/PhotosGallery.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+export default function PhotosGallery() {
+
+    // creates rows 
+    const rows = [
+        {
+            rowId = "row-1",
+            images: [
+            {id: "1", position: 1, url:"https://f005.backblazeb2.com/file/DarthPhotos/vogue+models+near+south+street+seaport+(1)_result.webp", desc: "New York models walking on the street"},
+            {id: "2", position: 3, url:"https://f005.backblazeb2.com/file/DarthPhotos/washington+square+park+astrolita+arch+(1+of+1).webp", desc: "Model in Washington Sqaure Park"},
+            {id: "3", position: 2, url:"https://f005.backblazeb2.com/file/DarthPhotos/DarthCornelius4484--0035_34A_result.webp", desc: "New Yorkers posing on the street"},
+            ]
+        },
+        {
+            rowId = "row-2",
+            images: [
+                { id: "4", position: 1, url:"https://f005.backblazeb2.com/file/DarthPhotos/Busted+3_result.webp", desc: "Lady in black latex"},
+                { id: "5", position: 2, url:"https://f005.backblazeb2.com/file/DarthPhotos/07_fashion.webp", desc: "B&W of a latino couple"},
+                { id: "6", position: 3, url:"https://f005.backblazeb2.com/file/DarthPhotos/Busted+3_result.webp", desc: "Girl in pink coat"},
+
+            ]
+        }
+        
+
+        
+    ]
+
+}
+
--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,18 +1,20 @@
 import { Link } from 'react-router-dom'
 import React from "react";
-import NavBar from './components/NavBar'
+import NavBar from '../components/NavBar'
 
 
 function Portfolio() {
     return (
         <div style= {{
             backgroundColor:'#444444', 
-
         }}
         >
 
-            <h1>Portfolio Page</h1>
-        </div>
+        <NavBar />
+        </div> 
+
+        
+
     )
 }
 
